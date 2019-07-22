dc6315851e7946899b13f5a104c6db79789953db
sages2003 <52415725+sages2003@users.noreply.github.com>
2019-07-22 16:37:46 -0400

    Add files via upload

---
+++ samstagram2/profile.js
@@ -0,0 +1,17 @@
+﻿$("#edit_profile").submit(function(e) {
+    e.preventDefault();
+    
+    var namechange = document.getElementById("newnameinput").value;
+    var photochange = document.getElementById("newphotoinput").value;
+    var biochange = document.getElementById("newbioinput").value;
+    var save = document.getElementById("savebutton").value;
+
+    document.getElementById("og-name-text").innerHTML = document.getElementById("newnameinput").value;
+    document.getElementById("og-profile-pic").src = document.getElementById("newphotoinput").value;
+    document.getElementById("og-bio").innerHTML = document.getElementById("newbioinput").value;
+
+    newnameinput.value="";
+    newphotoinput.value="";
+    newbioinput.value="";
+
+});
---
+++ samstagram2/samstagramindex.js
@@ -0,0 +1,33 @@
+﻿$("#post").submit(function (e) {
+    e.preventDefault();
+    console.log("posted!!");
+
+    var image_input = document.getElementById("urlimage_input");
+    var image_post = image_input.value;
+    var image_cap = document.getElementById("caption_input");
+    var cap_text = image_cap.value;
+
+
+    var post_input = document.createElement("div");
+    post_input.classList.add("post_input");
+    var cap_text_elem = document.createElement("p");
+    cap_text_elem.innerHTML = caption_input.value;
+    var image = document.createElement("img");
+    image.src = image_post;
+    console.log(image);
+    var x = document.createElement("p");
+    x.id = "a";
+    x.innerHTML = "X";
+    post_input.appendChild(x);
+    post_input.appendChild(image);
+    post_input.appendChild(cap_text_elem); ///the troublesome line///
+    document.getElementById("container").appendChild(post_input);
+    x.addEventListener("click", function (){
+        var container_id = String(Math.random());
+        post_input.id = container_id;
+        document.getElementById(container_id).remove();
+    });
+
+
+    
+});
